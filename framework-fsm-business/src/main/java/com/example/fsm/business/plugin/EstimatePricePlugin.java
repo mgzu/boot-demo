43b26177394e75b3068fe045dbba2ca63a2ff6ab
马光祖 <2931170625@qq.com>
2021-12-30 18:17:33 +0800

    refactor: 完善 fsm 插件机制

--- framework-fsm-business/src/main/java/com/example/fsm/business/plugin/EstimatePricePlugin.java
+++ framework-fsm-business/src/main/java/com/example/fsm/business/plugin/EstimatePricePlugin.java
@@ -1,24 +1,36 @@
 package com.example.fsm.business.plugin;
 
-
-import com.example.fsm.business.context.CreateOrderContext;
 import com.example.fsm.ServiceResult;
 import com.example.fsm.annotation.ProcessorPlugin;
+import com.example.fsm.business.context.CreateOrderContext;
+import com.example.fsm.business.enums.BizCodeEnum;
 import com.example.fsm.business.enums.OrderEventEnum;
 import com.example.fsm.business.enums.OrderStateEnum;
+import com.example.fsm.business.enums.SceneIdEnum;
 import com.example.fsm.context.StateContext;
 import com.example.fsm.plugin.PluginHandler;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 预估价插件
  */
-@ProcessorPlugin(state = OrderStateEnum.INIT, event = OrderEventEnum.CREATE, bizCode = "BUSINESS")
+@Slf4j
+@ProcessorPlugin(
+        state = OrderStateEnum.INIT,
+        event = OrderEventEnum.CREATE,
+        bizCode = {
+                BizCodeEnum.FBA,
+                BizCodeEnum.CARGO,
+        },
+        sceneId = SceneIdEnum.H5
+)
 public class EstimatePricePlugin implements PluginHandler<String, CreateOrderContext> {
     @Override
     public ServiceResult action(StateContext<CreateOrderContext> context) throws Exception {
+        log.info("EstimatePricePlugin action");
 //        String price = priceSerive.getPrice();
         String price = "";
         context.getContext().setEstimatePriceInfo(price);
-        return new ServiceResult();
+        return new ServiceResult(true);
     }
 }
--- framework-fsm-core/src/main/java/com/example/fsm/engine/StateProcessRegistry.java
+++ framework-fsm-core/src/main/java/com/example/fsm/engine/StateProcessRegistry.java
@@ -6,10 +6,22 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 /**
+ * 状态处理器注册器
+ *
  * @author MaGuangZu
  * @since 2021-09-26
  */
 public interface StateProcessRegistry {
+
+    /**
+     * 根据条件获取状态处理器
+     *
+     * @param orderState 订单状态
+     * @param eventType  事件类型
+     * @param bizCode    业务代码
+     * @param sceneId    场景ID
+     * @return 符合条件的处理器
+     */
     @NotNull
     List<AbstractStateProcessor> acquireStateProcess(String orderState, String eventType, String bizCode, String sceneId);
 }
