82343454773b046cb860dab33a2d1faa961fec88
mgzu <2931170625@qq.com>
2021-12-30 18:17:33 +0800

    refactor: 完善 fsm 插件机制

---
+++ framework-fsm-core/src/main/java/com/example/fsm/engine/StatePluginRegistry.java
@@ -0,0 +1,28 @@
+package com.example.fsm.engine;
+
+import com.example.fsm.plugin.PluginHandler;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * 插件注册器
+ *
+ * @author MaGuangZu
+ * @since 2021-12-30
+ */
+public interface StatePluginRegistry {
+
+    /**
+     * 根据条件获取插件
+     *
+     * @param orderState 订单状态
+     * @param eventType  事件类型
+     * @param bizCode    业务代码
+     * @param sceneId    场景ID
+     * @return 符合条件的插件列表
+     */
+    @NotNull
+    List<PluginHandler> acquireStatePlugin(String orderState, String eventType, String bizCode, String sceneId);
+
+}
--- framework-fsm-core/src/main/java/com/example/fsm/plugin/PluginExecutor.java
+++ framework-fsm-core/src/main/java/com/example/fsm/plugin/PluginExecutor.java
@@ -1,21 +1,56 @@
 package com.example.fsm.plugin;
 
+import com.example.fsm.FsmOrder;
+import com.example.fsm.ServiceResult;
 import com.example.fsm.context.StateContext;
+import com.example.fsm.engine.StatePluginRegistry;
+import com.example.fsm.exception.FsmException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * @author MaGuangZu
  * @since 2021-09-26
  */
+@Slf4j
 @Component
 public class PluginExecutor {
 
     private ExecutorService executor = Executors.newSingleThreadExecutor();
 
-    public <C> void parallelExecutor(StateContext<C> context) {
+    @Autowired
+    private StatePluginRegistry statePluginRegistry;
 
+    public <C> void parallelExecutor(StateContext<C> context) {
+        FsmOrder fsmOrder = context.getFsmOrder();
+        List<PluginHandler> handlerList = statePluginRegistry.acquireStatePlugin(fsmOrder.getOrderState(),
+                context.getOrderStateEvent().getEventType(), fsmOrder.getBizCode(), fsmOrder.getSceneId());
+        if (!handlerList.isEmpty()) {
+            List<Future<ServiceResult>> resultList = Collections.synchronizedList(new ArrayList<>());
+            for (PluginHandler handler : handlerList) {
+                Future<ServiceResult> result = executor.submit(() -> handler.action(context));
+                resultList.add(result);
+            }
+            for (Future<ServiceResult> future : resultList) {
+                ServiceResult sr;
+                try {
+                    sr = future.get();
+                } catch (Exception e) {
+                    log.error("parallelCheck executor.submit error.", e);
+                    throw new FsmException(e.getCause());
+                }
+                if (!sr.isSuccess()) {
+                    throw new FsmException(sr.getMessage());
+                }
+            }
+        }
     }
 }
