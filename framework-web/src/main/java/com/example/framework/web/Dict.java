1e7605b074e21426d37b96192a5ee873e2b1dd48
mgzu <2931170625@qq.com>
2022-08-16 10:29:29 +0800

    feat: spring-boot 3

--- framework-web/src/main/java/com/example/framework/web/Dict.java
+++ framework-web/src/main/java/com/example/framework/web/Dict.java
@@ -1,10 +1,9 @@
 package com.example.framework.web;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 /**
  * @author MaGuangZu
