47351612c4acfa432c6c145d0b95314536657093
mgzu <2931170625@qq.com>
2022-08-19 15:23:02 +0800

    refactor: 优化，添加 @NotNull

--- framework-fsm-core/src/main/java/com/example/fsm/service/FsmOrderService.java
+++ framework-fsm-core/src/main/java/com/example/fsm/service/FsmOrderService.java
@@ -1,11 +1,12 @@
 package com.example.fsm.service;
 
 import com.example.fsm.FsmOrder;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author MaGuangZu
  * @since 2021-09-26
  */
 public interface FsmOrderService {
-    FsmOrder getFsmOrder(String orderId);
+    FsmOrder getFsmOrder(@NotNull String orderId);
 }
