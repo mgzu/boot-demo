518e11633d631aba0e40b83c76d87121e43a02f6
mgzu <2931170625@qq.com>
2022-12-02 12:05:28 +0800

    refactor: rule

---
+++ app-web-business/app-order/src/main/java/com/example/app/order/constants/RuleConstants.java
@@ -0,0 +1,12 @@
+package com.example.app.order.constants;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author MaGuangZu
+ * @since 2022-10-25
+ */
+@UtilityClass
+public class RuleConstants {
+    public static final String ORDER = "ORDER";
+}
