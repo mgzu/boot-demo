af5359036ec4d115238765bace571b090b9489dd
mgzu <2931170625@qq.com>
2022-12-02 15:35:19 +0800

    refactor: LocalDateTime json format

---
+++ framework-web/src/main/java/com/example/framework/web/configure/JsonConfigure.java
@@ -0,0 +1,45 @@
+package com.example.framework.web.configure;
+
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
+/**
+ * @author MaGuangZu
+ * @since 2022-12-02
+ */
+@Configuration
+public class JsonConfigure {
+
+    @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
+    private String pattern;
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilder() {
+        return builder -> {
+            TimeZone tz = TimeZone.getTimeZone("UTC");
+            DateFormat df = new SimpleDateFormat(pattern);
+            df.setTimeZone(tz);
+            builder.failOnEmptyBeans(false)
+                    .failOnUnknownProperties(false)
+                    .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                    .serializerByType(LocalDateTime.class, localDateTimeDeserializer())
+                    .dateFormat(df);
+        };
+    }
+
+    @Bean
+    public LocalDateTimeSerializer localDateTimeDeserializer() {
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
+    }
+
+}
