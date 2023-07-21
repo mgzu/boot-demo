51fa26bd66add4bf1c4d0cdfb2acec8abd1fcc1c
马光祖 <2931170625@qq.com>
2023-07-21 17:07:07 +0800

    refactor: optimize fsm engine

--- framework-fsm-core/src/main/java/com/example/fsm/processor/StateProcessor.java
+++ framework-fsm-core/src/main/java/com/example/fsm/processor/StateProcessor.java
@@ -3,6 +3,7 @@ package com.example.fsm.processor;
 import com.example.fsm.ServiceResult;
 import com.example.fsm.context.StateContext;
 import com.example.fsm.exception.FsmException;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * 状态机处理器接口
@@ -11,5 +12,6 @@ public interface StateProcessor<T, C> {
 	/**
 	 * 执行状态迁移的入口
 	 */
-	ServiceResult<T> action(StateContext<C> context) throws FsmException;
+	@NotNull
+	ServiceResult<T> action(@NotNull StateContext<C> context) throws FsmException;
 }
