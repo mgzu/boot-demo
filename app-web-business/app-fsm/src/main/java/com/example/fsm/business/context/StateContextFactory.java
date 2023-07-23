d8d8f0cd02f2a77fe2c484938f0f8d5f46a34e05
mgzu <2931170625@qq.com>
2023-07-23 20:07:15 +0800

    refactor: optimize fsm engine

--- app-web-business/app-fsm/src/main/java/com/example/fsm/business/context/StateContextFactory.java
+++ app-web-business/app-fsm/src/main/java/com/example/fsm/business/context/StateContextFactory.java
@@ -12,26 +12,26 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 
-@SuppressWarnings("java:S3740")
 @UtilityClass
 public class StateContextFactory {
 
-	static final Map<String, BiFunction<OrderStateEvent, FsmOrder, StateContext>> contextMap = new HashMap<>();
+	static final Map<String, BiFunction<OrderStateEvent, FsmOrder, StateContext<?>>> contextMap = new HashMap<>();
 
 	static {
 		contextMap.put(CreateEvent.class.getName(), (orderStateEvent, fsmOrder) -> new CreateOrderContext((CreateEvent) orderStateEvent, fsmOrder));
 		contextMap.put(ReturnEvent.class.getName(), (orderStateEvent, fsmOrder) -> new ReturnOrderContext((ReturnEvent) orderStateEvent, fsmOrder));
 	}
 
-	@SuppressWarnings("unchecked")
 	@NotNull
 	public static <C> StateContext<C> create(@NotNull OrderStateEvent orderStateEvent, @NotNull FsmOrder fsmOrder) {
 		String klassName = orderStateEvent.getClass().getName();
-		BiFunction<OrderStateEvent, FsmOrder, StateContext> function = contextMap.get(klassName);
+		BiFunction<OrderStateEvent, FsmOrder, StateContext<?>> function = contextMap.get(klassName);
 		if (function == null) {
 			throw new IllegalArgumentException("unsupported event class:" + klassName);
 		}
-		return function.apply(orderStateEvent, fsmOrder);
+		@SuppressWarnings("unchecked")
+		StateContext<C> context = (StateContext<C>) function.apply(orderStateEvent, fsmOrder);
+		return context;
 	}
 
 }
