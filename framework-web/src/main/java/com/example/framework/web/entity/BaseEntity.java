4b0d81ad64280836186adb62b054c7d0b111e378
mgzu <2931170625@qq.com>
2023-08-30 10:27:48 +0800

    refactor: optimize BaseEntity

--- framework-web/src/main/java/com/example/framework/web/entity/BaseEntity.java
+++ framework-web/src/main/java/com/example/framework/web/entity/BaseEntity.java
@@ -1,13 +1,13 @@
 package com.example.framework.web.entity;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -31,24 +31,27 @@ public class BaseEntity extends PersistableEntity {
 
 	@NotNull
     @CreatedBy
-	@Column(nullable = false)
+	@Column(nullable = false, updatable = false)
     private String createdBy;
 
 	@NotNull
     @CreatedDate
     @Temporal(TemporalType.TIMESTAMP)
-	@Column(nullable = false)
+	@Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;
 
-	@Nullable
+	@NotNull
     @LastModifiedBy
+	@Column(nullable = false)
     private String lastModifiedBy;
 
-	@Nullable
+	@NotNull
     @LastModifiedDate
     @Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
     private LocalDateTime lastModifiedDate;
 
+	@Nullable
 	private String remark;
 
 	@NotNull
