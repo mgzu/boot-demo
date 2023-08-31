beb50d477c065e4646a5fd4954724f58f2a3b0a2
马光祖 <2931170625@qq.com>
2023-08-31 15:34:59 +0800

    refactor: clean code

--- framework-system/src/main/java/com/example/framework/system/entity/UserRole.java
+++ framework-system/src/main/java/com/example/framework/system/entity/UserRole.java
@@ -3,7 +3,6 @@ package com.example.framework.system.entity;
 import com.example.framework.web.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
