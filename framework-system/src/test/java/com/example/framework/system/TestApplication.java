19229d2d7981e6eeee402e89acc90eba25703aa9
马光祖 <2931170625@qq.com>
2023-08-31 15:36:16 +0800

    feat: add system repository unit test

---
+++ framework-system/src/test/java/com/example/framework/system/TestApplication.java
@@ -0,0 +1,11 @@
+package com.example.framework.system;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MaGuangZu
+ * @since 2023-08-31
+ */
+@SpringBootApplication
+public class TestApplication {
+}
