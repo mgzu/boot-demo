4a548655cb96170d90eef26eba608bb9f43c4ac2
mgzu <2931170625@qq.com>
2023-08-31 21:55:11 +0800

    feat: mapstruct shadow clone and deep clone example

--- framework-test/src/test/java/com/example/framework/testsupport/converts/ProductConvertTest.java
+++ framework-test/src/test/java/com/example/framework/testsupport/converts/ProductConvertTest.java
@@ -1,10 +1,13 @@
 package com.example.framework.testsupport.converts;
 
 import com.example.framework.testsupport.entity.Product;
+import com.example.framework.testsupport.entity.Stock;
 import com.google.common.collect.Lists;
 import org.dromara.hutool.core.data.id.IdUtil;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,57 +17,49 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class ProductConvertTest {
 
-	@Disabled("don't know mapstruct shadow copy")
-	@Test
-	void testShadowClone() {
-		var product = Product.builder()
-			.id(IdUtil.getSnowflakeNextIdStr())
-			.name("product")
-			.stores(Lists.newArrayList("store1", "store2"))
-			.build();
-
+	@MethodSource("getProduct")
+	@ParameterizedTest
+	void shadowCloneTest(Product product) {
 		var copy = ProductConvert.INSTANCE.clone(product);
 
-		assertThat(product.getId()).isEqualTo(copy.getId());
-		assertThat(product.getName()).isEqualTo(copy.getName());
-		assertThat(product.getStores().get(0)).isEqualTo(copy.getStores().get(0));
-		assertThat(product.getStores().get(1)).isEqualTo(copy.getStores().get(1));
+		assertThat(product).isEqualTo(copy);
 
 		copy.setId(IdUtil.getSnowflakeNextIdStr());
 		copy.setName("new str");
-		copy.getStores().set(0, "new store1");
-		copy.getStores().set(1, "new store2");
+		copy.getStocks().get(0).setName("new stock1");
+		copy.getStocks().get(1).setName("new stock2");
 
 		assertThat(product.getId()).isNotEqualTo(copy.getId());
 		assertThat(product.getName()).isNotEqualTo(copy.getName());
-		assertThat(product.getStores().get(0)).isEqualTo(copy.getStores().get(0));
-		assertThat(product.getStores().get(1)).isEqualTo(copy.getStores().get(1));
+		assertThat(product.getStocks()).isEqualTo(copy.getStocks());
 	}
 
-	@Test
-	void testDeepClone() {
-		var product = Product.builder()
-			.id(IdUtil.getSnowflakeNextIdStr())
-			.name("product")
-			.stores(Lists.newArrayList("store1", "store2"))
-			.build();
-
+	@MethodSource("getProduct")
+	@ParameterizedTest
+	void deepCloneTest(Product product) {
 		var copy = ProductConvert.INSTANCE.deepClone(product);
 
-		assertThat(product.getId()).isEqualTo(copy.getId());
-		assertThat(product.getName()).isEqualTo(copy.getName());
-		assertThat(product.getStores().get(0)).isEqualTo(copy.getStores().get(0));
-		assertThat(product.getStores().get(1)).isEqualTo(copy.getStores().get(1));
+		assertThat(product).isEqualTo(copy);
 
 		copy.setId(IdUtil.getSnowflakeNextIdStr());
 		copy.setName("new str");
-		copy.getStores().set(0, "new store1");
-		copy.getStores().set(1, "new store2");
+		copy.getStocks().get(0).setName("new stock1");
+		copy.getStocks().get(1).setName("new stock2");
 
 		assertThat(product.getId()).isNotEqualTo(copy.getId());
 		assertThat(product.getName()).isNotEqualTo(copy.getName());
-		assertThat(product.getStores().get(0)).isNotEqualTo(copy.getStores().get(0));
-		assertThat(product.getStores().get(1)).isNotEqualTo(copy.getStores().get(1));
+		assertThat(product.getStocks()).isNotEqualTo(copy.getStocks());
+	}
+
+	static List<Product> getProduct() {
+		return List.of(Product.builder()
+			.id(IdUtil.getSnowflakeNextIdStr())
+			.name("product")
+			.stocks(Lists.newArrayList(
+				Stock.builder().id("1").name("stock1").build(),
+				Stock.builder().id("2").name("stock2").build()
+			))
+			.build());
 	}
 
 }
