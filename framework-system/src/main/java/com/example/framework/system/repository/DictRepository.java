4f0d4a23811fb51451df5d8a47b9724eb5c7473e
马光祖 <2931170625@qq.com>
2023-09-06 12:19:56 +0800

    refactor: Remove redundant annotations

--- framework-system/src/main/java/com/example/framework/system/repository/DictRepository.java
+++ framework-system/src/main/java/com/example/framework/system/repository/DictRepository.java
@@ -2,8 +2,6 @@ package com.example.framework.system.repository;
 
 import com.example.framework.system.entity.Dict;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DictRepository extends JpaRepository<Dict, String> {
 }
--- framework-system/src/main/java/com/example/framework/system/repository/RolePermissionRepository.java
+++ framework-system/src/main/java/com/example/framework/system/repository/RolePermissionRepository.java
@@ -2,8 +2,6 @@ package com.example.framework.system.repository;
 
 import com.example.framework.system.entity.RolePermission;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RolePermissionRepository extends JpaRepository<RolePermission, String> {
 }
