fa6f5f98ca4293565d52daa32d7a85ef26269799
mgzu <2931170625@qq.com>
2023-10-20 00:19:23 +0800

    refactor: remove useless annotation

--- app-web-business/app-fsm/src/main/java/com/example/fsm/business/repository/OrderRepository.java
+++ app-web-business/app-fsm/src/main/java/com/example/fsm/business/repository/OrderRepository.java
@@ -2,12 +2,10 @@ package com.example.fsm.business.repository;
 
 import com.example.app.common.entity.Order;
 import com.example.framework.web.configure.jpa.repositories.TenantRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author MaGuangZu
  * @since 2021-12-27
  */
-@Repository
 public interface OrderRepository extends TenantRepository<Order> {
 }
