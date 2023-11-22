5a4161b4623f2ba91580e843e5e9aadd0e70e539
mgzu <2931170625@qq.com>
2023-11-22 15:20:10 +0800

    feat(wip): TransactionConfigure

---
+++ framework-web/src/main/java/com/example/framework/web/configure/TransactionConfigure.java
@@ -0,0 +1,20 @@
+package com.example.framework.web.configure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author MaGuangZu
+ * @since 2023-11-22
+ */
+@Configuration
+public class TransactionConfigure {
+
+	// TODO: customizer RollbackRuleAttribute
+//	@Bean
+//	public PlatformTransactionManagerCustomizer<JtaTransactionManager> transactionManagerCustomizer() {
+//		return customizer -> {
+//			customizer.rollback();
+//		};
+//	}
+
+}
