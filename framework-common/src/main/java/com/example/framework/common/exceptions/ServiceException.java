89019d84aeda5574f323192c0c0fcb73a963a2e5
mgzu <2931170625@qq.com>
2023-11-23 15:03:18 +0800

    refactor: optimize

--- framework-common/src/main/java/com/example/framework/common/exceptions/ServiceException.java
+++ framework-common/src/main/java/com/example/framework/common/exceptions/ServiceException.java
@@ -2,12 +2,16 @@ package com.example.framework.common.exceptions;
 
 public class ServiceException extends RuntimeException {
 
-	public ServiceException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
 	public ServiceException(String message) {
 		super(message);
+	}
+
+	public ServiceException(Exception e) {
+		super(e);
+	}
+
+	public ServiceException(String message, Throwable cause) {
+		super(message, cause);
 	}
 
 }
