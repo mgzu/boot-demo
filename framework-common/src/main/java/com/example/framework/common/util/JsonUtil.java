dc35867e44adf83b149cbc03ba188864217a9bc1
mgzu <2931170625@qq.com>
2024-12-05 20:10:36 +0800

    build(deps): bump hutool from 6.0.0-M16 to 6.0.0-M18

--- framework-common/src/main/java/com/example/framework/common/util/JsonUtil.java
+++ framework-common/src/main/java/com/example/framework/common/util/JsonUtil.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
-import org.dromara.hutool.core.date.DatePattern;
+import org.dromara.hutool.core.date.DateFormatPool;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -20,11 +20,11 @@ public class JsonUtil {
 
 	static {
 		SimpleModule simpleModule = new SimpleModule();
-		simpleModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATETIME_PATTERN)));
+		simpleModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DateFormatPool.NORM_DATETIME_PATTERN)));
 
 		objectMapper.findAndRegisterModules();
 		objectMapper.registerModule(simpleModule);
-		objectMapper.setDateFormat(new SimpleDateFormat(DatePattern.NORM_DATETIME_PATTERN));
+		objectMapper.setDateFormat(new SimpleDateFormat(DateFormatPool.NORM_DATETIME_PATTERN));
 	}
 
 	@SneakyThrows
